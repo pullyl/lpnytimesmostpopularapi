8b6848556c66396071594a6689e84d6b30fd8724
Lauren Pully <pullyl@gmail.com>
2016-05-09 16:56:53 -0400

    Initial Commit

---
+++ nytimesmostpopularapi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nytimesmostpopularapi
+//
+//  Created by Lauren Pully on 5/9/16.
+//  Copyright © 2016 laurenpully. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
