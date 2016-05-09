afbd7b1d36950d0cd211921b7be99942013fb9bc
Lauren Pully <pullyl@gmail.com>
2016-05-09 18:19:40 -0500

    added Swifty.JSON added basic json reading added hello world text

--- nytimesmostpopularapi/ViewController.swift
+++ nytimesmostpopularapi/ViewController.swift
@@ -7,12 +7,60 @@
 //
 
 import UIKit
+import Foundation
 
 class ViewController: UIViewController {
+    
+    var objects = [[String: String]]()
 
-    override func viewDidLoad() {
+    
+    func parseJSON(json: JSON) {
+        for result in json["results"].arrayValue {
+            let title = result["title"].stringValue
+            let byline = result["byline"].stringValue
+            let obj = ["title": title, "byline": byline]
+            objects.append(obj)
+        }
+        
+        print("done parsing json")
+        
+        //tableView.reloadData()
+    }
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        print ("viewDidLoad")
+        
+        //read in API
+        let apiURLString = "http://api.nytimes.com/svc/mostpopular/v2/mostviewed/all-sections/7.json?api-key=98fa23b7d5b542f2be105b8384512928"
+        
+        print ("viewDidLoad set apiURLString to: ", apiURLString)
+        
+        if let url = NSURL(string: apiURLString) {
+            if let data = try? NSData(contentsOfURL: url, options: []) {
+                let json = JSON(data: data)
+                
+                if json["status"] == "OK" {
+                    parseJSON(json)
+                }
+                
+                else{
+                    print("error parsing JSON")
+                }
+            }
+        }
+    
+    
+        //set text on screen
+        let label = UILabel(frame: CGRectMake(0, 100, 200, 21))
+        label.center = CGPointMake(160, 284)
+        label.textAlignment = NSTextAlignment.Center
+        label.text = "hello world"
+        self.view.addSubview(label)
+        
+        print("end viewDidLoad")
     }
 
     override func didReceiveMemoryWarning() {
